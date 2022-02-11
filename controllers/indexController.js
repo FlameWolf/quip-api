9bba1b93967cd766029dc0e9fabb0eb4e3002105
FlameWolf <ajifocus@LIVE.COM>
2022-02-11 15:35:06 +0530

    first commit

---
+++ controllers/indexController.js
@@ -0,0 +1,5 @@
+const getIndex = (req, res, next) => {
+	res.send("Hello, World!");
+};
+
+module.exports = { getIndex };
---
+++ controllers/userController.js
@@ -0,0 +1,5 @@
+const getUsers = function (req, res, next) {
+	res.send("Respond with a resource");
+};
+
+module.exports = { getUsers };
