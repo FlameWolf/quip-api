d388274dd6d0714a09de51911d6c044bda6bb937
FlameWolf <ajifocus@LIVE.COM>
2022-02-11 19:01:29 +0530

    Add database connection

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const mongoose = require("mongoose");
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
@@ -8,6 +9,15 @@ const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 
 const app = express();
+
+mongoose
+	.connect("mongodb://localhost:27017/?readPreference=primary&ssl=false")
+	.then(() => {
+		console.log("Connected to the database");
+	})
+	.catch(() => {
+		console.log("Unable to connect to the database");
+	});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
