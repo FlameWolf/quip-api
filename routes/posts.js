878f3d908a444479796e9965715c93d8e7d050c4
FlameWolf <ajifocus@LIVE.COM>
2022-02-12 18:37:56 +0530

    Update logic for post routes

--- routes/posts.js
+++ routes/posts.js
@@ -2,9 +2,6 @@
 
 const express = require("express");
 const router = express.Router();
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const { invalidHandles, handleRegExp, passwordRegExp, rounds, timeout, JWT_SECRET, authCookieName } = require("../library");
 const generalController = require("../controllers/general.controller");
 const Post = require("../models/post.model");
 
@@ -21,25 +18,74 @@ router.post("/create", async (req, res, next) => {
 	const model = new Post({ content, author, repeatPost, replyTo });
 	try {
 		const post = await model.save();
-		generalController.successResponse(res, 201, createPostAction, { post });
+		generalController.successResponse(res, 201, createPostAction, post);
 	} catch (err) {
-		generalController.failureResponse(res, 400, createPostAction, err.message);
+		generalController.failureResponse(res, 500, createPostAction, err.message);
 	}
 });
 router.post("/favourite", async (req, res, next) => {
-	const favouriteAction = "Add favourite";
+	const favouritePostAction = "Add favourite";
+	const postId = req.body.postId;
+	const userId = req.body.userId;
+	try {
+		const result = await Post.findByIdAndUpdate(postId, { $addToSet: { favouritedBy: userId } });
+		generalController.successResponse(res, 200, favouritePostAction, result);
+	} catch (err) {
+		generalController.failureResponse(res, 500, favouritePostAction, err.message);
+	}
 });
 router.post("/unfavourite", async (req, res, next) => {
-	const unfavouriteAction = "Remove favourite";
+	const unfavouritePostAction = "Remove favourite";
+	const postId = req.body.postId;
+	const userId = req.body.userId;
+	try {
+		const result = await Post.findByIdAndUpdate(postId, { $pull: { favouritedBy: userId } });
+		generalController.successResponse(res, 200, unfavouritePostAction, result);
+	} catch (err) {
+		generalController.failureResponse(res, 500, unfavouritePostAction, err.message);
+	}
 });
 router.post("/repeat", async (req, res, next) => {
-	const repeatAction = "Repeat";
+	const repeatPostAction = "Repeat";
+	const postId = req.body.postId;
+	const userId = req.body.userId;
+	const payload = {
+		author: userId,
+		repeatPost: postId
+	};
+	try {
+		if (await Post.find(payload)) {
+			await Post.deleteOne(payload);
+		}
+		const post = await new Post(payload).save();
+		generalController.successResponse(res, 201, repeatPostAction, post);
+	} catch (err) {
+		generalController.failureResponse(res, 500, repeatPostAction, err.message);
+	}
 });
 router.post("/unrepeat", async (req, res, next) => {
-	const unrepeatAction = "Undo repeat";
+	const unrepeatPostAction = "Undo repeat";
+	const postId = req.body.postId;
+	const userId = req.body.userId;
+	try {
+		const result = await Post.deleteOne({
+			author: userId,
+			repeatPost: postId
+		});
+		generalController.successResponse(res, 201, unrepeatPostAction, result);
+	} catch (err) {
+		generalController.failureResponse(res, 500, unrepeatPostAction, err.message);
+	}
 });
 router.post("/delete", async (req, res, next) => {
-	const deleteAction = "Delete post";
+	const deletePostAction = "Delete post";
+	const postId = req.body.postId;
+	try {
+		const result = await Post.findByIdAndDelete(postId);
+		generalController.successResponse(res, 200, deletePostAction, result);
+	} catch (err) {
+		generalController.failureResponse(res, 500, deletePostAction, err.message);
+	}
 });
 
 module.exports = router;
