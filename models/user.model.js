bc608f7d16b88f22f5a3e84a803065e2529491b0
FlameWolf <ajifocus@LIVE.COM>
2022-02-15 22:08:25 +0530

    Add mute word routes

--- models/user.model.js
+++ models/user.model.js
@@ -4,15 +4,6 @@ const { ObjectId } = require("bson");
 const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
 
-const wordSchema = new mongoose.Schema({
-	word: { type: String, trim: true },
-	match: { type: String, enum: ["exact", "startsWith", "endsWith", "contains"] }
-});
-const muteListSchema = new mongoose.Schema({
-	users: [{ type: ObjectId, ref: "User" }],
-	words: [{ type: wordSchema }],
-	posts: [{ type: ObjectId, ref: "Post" }]
-});
 const userSchema = new mongoose.Schema(
 	{
 		handle: { type: String, trim: true, required: true, unique: true },
@@ -21,7 +12,20 @@ const userSchema = new mongoose.Schema(
 		favourites: [{ type: ObjectId, ref: "Post", select: false }],
 		following: [{ type: ObjectId, ref: "User", select: false }],
 		followers: [{ type: ObjectId, ref: "User", select: false }],
-		muteList: { type: muteListSchema, default: {}, select: false },
+		muteList: {
+			type: new mongoose.Schema({
+				users: [{ type: ObjectId, ref: "User" }],
+				words: [
+					{
+						word: { type: String, trim: true },
+						match: { type: String, enum: ["exact", "startsWith", "endsWith", "contains"] }
+					}
+				],
+				posts: [{ type: ObjectId, ref: "Post" }]
+			}),
+			default: {},
+			select: false
+		},
 		blockList: [{ type: ObjectId, ref: "User", select: false }],
 		isDeactivated: { type: Boolean, default: false },
 		isDeleted: { type: Boolean, default: false }
