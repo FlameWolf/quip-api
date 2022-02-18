c506b310225ad36f14ce16c80f4b1297fcdd5ef2
FlameWolf <ajifocus@LIVE.COM>
2022-02-18 11:00:59 +0530

    Update library to load JWT secret from environment

--- library.js
+++ library.js
@@ -5,7 +5,7 @@ const handleRegExp = /^[A-Za-z][\w]{3,31}$/;
 const passwordRegExp = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
 const rounds = 10;
 const timeout = 1000 * 3600 * 24 * 7;
-const jwtSecret = "22b79d62c4273ab9ff785f55dc16615e";
+const jwtSecret = process.env.JWT_SECRET;
 const authCookieName = "userId";
 const contentLengthRegExp = /\p{L}\p{M}?|\S|\s/gu;
 const maxContentLength = 256;
