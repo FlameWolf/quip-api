5d053da49ac45d02889fe04528cf90316786a431
FlameWolf <ajifocus@LIVE.COM>
2022-02-22 21:10:58 +0530

    Refactor code

--- routes/users.js
+++ routes/users.js
@@ -3,15 +3,15 @@
 const express = require("express");
 const router = express.Router();
 const authenticateRequest = require("../middleware/authenticate-request");
-const userController = require("../controllers/users.controller");
+const usersController = require("../controllers/users.controller");
 
-router.get("/:handle", userController.getUser);
-router.get("/:handle/profile", userController.getUserProfile);
-router.get("/follow/:handle", authenticateRequest, userController.followUser);
-router.get("/unfollow/:handle", authenticateRequest, userController.unfollowUser);
-router.get("/mute/:handle", authenticateRequest, userController.muteUser);
-router.get("/unmute/:handle", authenticateRequest, userController.unmuteUser);
-router.get("/block/:handle", authenticateRequest, userController.blockUser);
-router.get("/unblock/:handle", authenticateRequest, userController.unblockUser);
+router.get("/:handle", usersController.getUser);
+router.get("/:handle/profile", usersController.getUserProfile);
+router.get("/follow/:handle", authenticateRequest, usersController.followUser);
+router.get("/unfollow/:handle", authenticateRequest, usersController.unfollowUser);
+router.get("/mute/:handle", authenticateRequest, usersController.muteUser);
+router.get("/unmute/:handle", authenticateRequest, usersController.unmuteUser);
+router.get("/block/:handle", authenticateRequest, usersController.blockUser);
+router.get("/unblock/:handle", authenticateRequest, usersController.unblockUser);
 
 module.exports = router;
