a52974662e2deda30b30e661db4e1e6b1db3fedd
FlameWolf <ajifocus@LIVE.COM>
2022-02-23 14:01:27 +0530

    Remove unnecessary indexing

--- models/mute.user.model.js
+++ models/mute.user.model.js
@@ -4,12 +4,10 @@ const { ObjectId } = require("bson");
 const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
 
-const muteUserSchema = new mongoose.Schema(
-	{
-		user: { type: ObjectId, ref: "User", index: true },
-		mutedBy: { type: ObjectId, ref: "User", index: true }
-	}
-);
+const muteUserSchema = new mongoose.Schema({
+	user: { type: ObjectId, ref: "User" },
+	mutedBy: { type: ObjectId, ref: "User", index: true }
+});
 muteUserSchema.index({ user: 1, mutedBy: 1 }, { unique: true, uniqueCaseInsensitive: true });
 muteUserSchema.plugin(uniqueValidator);
 
