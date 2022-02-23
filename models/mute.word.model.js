60b8b6521ffa31abf46c5eb30bfc4699988dffd1
FlameWolf <ajifocus@LIVE.COM>
2022-02-23 13:59:31 +0530

    Update mute word schema

--- models/mute.word.model.js
+++ models/mute.word.model.js
@@ -5,16 +5,11 @@ const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
 
 const muteWordSchema = new mongoose.Schema({
-	word: {
-		type: new mongoose.Schema({
-			word: { type: String, trim: true },
-			match: { type: String, enum: ["exact", "contains", "startsWith", "endsWith"] }
-		}),
-		index: true
-	},
+	word: { type: String, trim: true },
+	match: { type: String, enum: ["exact", "contains", "startsWith", "endsWith"] },
 	mutedBy: { type: ObjectId, ref: "User", index: true }
 });
-muteWordSchema.index({ word: 1, mutedBy: 1 }, { unique: true, uniqueCaseInsensitive: true });
+muteWordSchema.index({ word: 1, match: 1, mutedBy: 1 }, { unique: true, uniqueCaseInsensitive: true });
 muteWordSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model("MuteWord", muteWordSchema);
