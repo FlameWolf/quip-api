bdd4532e914af758c483bb8484217f5f150a0109
FlameWolf <ajifocus@LIVE.COM>
2022-02-23 14:05:45 +0530

    Remove unnecessary index

--- models/block.model.js
+++ models/block.model.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
 
 const blockSchema = new mongoose.Schema({
-	user: { type: ObjectId, ref: "User", index: true },
+	user: { type: ObjectId, ref: "User" },
 	blockedBy: { type: ObjectId, ref: "User", index: true }
 });
 blockSchema.index({ user: 1, blockedBy: 1 }, { unique: true, uniqueCaseInsensitive: true });
