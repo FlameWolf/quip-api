27e4a9ebc789637b48986903ddd0f36a2e9e7113
FlameWolf <ajifocus@LIVE.COM>
2022-02-24 14:28:24 +0530

    Add content length validation and remove unwanted fields

--- models/post.model.js
+++ models/post.model.js
@@ -1,18 +1,23 @@
 "use strict";
 
+const { contentLengthRegExp, maxContentLength } = require("../library");
 const { ObjectId } = require("bson");
 const mongoose = require("mongoose");
 const mongoosePaginate = require("mongoose-paginate-v2");
 
 const postSchema = new mongoose.Schema(
 	{
-		content: { type: String, text: true },
+		content: {
+			type: String,
+			text: true,
+			validate: {
+				validator: value => value.match(contentLengthRegExp).length <= maxContentLength,
+				message: `Content length exceeds the maximum allowed limit of ${maxContentLength} characters`
+			}
+		},
 		author: { type: ObjectId, ref: "User", required: true, index: true },
 		repeatPost: { type: ObjectId, ref: "Post", index: true },
 		replyTo: { type: ObjectId, ref: "Post", index: true },
-		favouritedBy: [{ type: ObjectId, ref: "User", select: false }],
-		repeatedBy: [{ type: ObjectId, ref: "User", select: false }],
-		replies: [{ type: ObjectId, ref: "Post", select: false }],
 		pinned: { type: Boolean }
 	},
 	{
