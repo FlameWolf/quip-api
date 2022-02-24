5be9963c5b2d5895d1bf54cf33d262df601f533b
FlameWolf <ajifocus@LIVE.COM>
2022-02-24 14:27:03 +0530

    Add handle and password validation

--- models/user.model.js
+++ models/user.model.js
@@ -1,13 +1,32 @@
 "use strict";
 
-const { ObjectId } = require("bson");
+const { handleRegExp, passwordRegExp } = require("../library");
 const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
 
 const userSchema = new mongoose.Schema(
 	{
-		handle: { type: String, trim: true, required: true, unique: true, uniqueCaseInsensitive: true },
-		password: { type: String, trim: true, required: true, select: false },
+		handle: {
+			type: String,
+			trim: true,
+			required: true,
+			unique: true,
+			uniqueCaseInsensitive: true,
+			validate: {
+				validator: value => handleRegExp.test(value),
+				message: "Handle is not valid"
+			}
+		},
+		password: {
+			type: String,
+			trim: true,
+			required: true,
+			select: false,
+			validate: {
+				validator: value => passwordRegExp.test(value),
+				message: "Password is not valid"
+			}
+		},
 		isDeactivated: { type: Boolean, default: false },
 		isDeleted: { type: Boolean, default: false }
 	},
