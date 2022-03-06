5c66c253e0954b762d4103db7249dcff3ef8a40b
FlameWolf <ajifocus@LIVE.COM>
2022-03-06 13:31:52 +0530

    Create multer.controller.js

---
+++ controllers/multer.controller.js
@@ -0,0 +1,22 @@
+const multer = require("multer");
+
+const validMimeTypes = ["image/", "video/"];
+const isMimeTypeValid = mimeType => validMimeTypes.some(x => mimeType.startsWith(x));
+const storage = multer.diskStorage({
+	destination: (req, file, callbackFn) => {
+		let error = null;
+		if (!isMimeTypeValid(file.mimetype)) {
+			error = new Error("Invalid file type");
+		}
+		callbackFn(error, "images");
+	},
+	filename: (httpRequest, file, callbackFn) => {
+		let fileName = file.originalname
+			.trim()
+			.replace(/\W+(?=.*\.[^.]*$)/g, "-")
+			.replace(/\.\w+$/, "");
+		callbackFn(null, `${fileName}-${Date.now().valueOf()}.${file.mimetype.split("/")[1]}`);
+	}
+});
+
+exports.extractMediaFile = multer({ storage });
