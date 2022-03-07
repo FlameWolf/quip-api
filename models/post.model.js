1b01ca0ee4a6fdd8ec7124f6ee390e3dcc72f650
FlameWolf <ajifocus@LIVE.COM>
2022-03-07 10:50:43 +0530

    Add location and geo-spatial index to post schema

--- models/post.model.js
+++ models/post.model.js
@@ -3,21 +3,23 @@
 const { contentLengthRegExp, maxContentLength } = require("../library");
 const { ObjectId } = require("bson");
 const mongoose = require("mongoose");
+const Point = require("../schemaTypes/point.schema.type");
 
 const postSchema = new mongoose.Schema(
 	{
 		content: {
 			type: String,
-			text: true,
 			validate: {
 				validator: value => value.match(contentLengthRegExp).length <= maxContentLength,
 				message: `Content length exceeds the maximum allowed limit`
-			}
+			},
+			index: "text"
 		},
 		author: { type: ObjectId, ref: "User", required: true, index: true },
 		repeatPost: { type: ObjectId, ref: "Post", index: true },
 		replyTo: { type: ObjectId, ref: "Post", index: true },
 		attachments: { type: ObjectId, ref: "Attachments" },
+		location: { type: Point, index: "2dsphere" },
 		pinned: { type: Boolean }
 	},
 	{
