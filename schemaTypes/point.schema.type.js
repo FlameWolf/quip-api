e716ae9df70735da6075332515b6a4f693dcd9ea
FlameWolf <ajifocus@LIVE.COM>
2022-03-12 10:30:21 +0530

    Update point.schema.type.js

--- schemaTypes/point.schema.type.js
+++ schemaTypes/point.schema.type.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 const ValidatorError = mongoose.Error.ValidatorError;
-const { validateCoordinates } = require("../library");
 
 class PointSchemaType extends mongoose.SchemaType {
 	constructor(key, options) {
@@ -8,14 +7,28 @@ class PointSchemaType extends mongoose.SchemaType {
 	}
 
 	cast(value) {
-		if (!value.type) {
-			throw new ValidatorError("Point", value.type, "point.type");
+		const { type, coordinates } = value;
+		if (!type) {
+			throw new ValidatorError("Point", this.constructor.name, "point.type");
 		}
-		if (value.type !== "Point") {
-			throw new ValidatorError(`${value.type} is not a valid GeoJSON type`);
+		if (type !== "Point") {
+			throw new ValidatorError(`Property "type" must be "Point"`);
 		}
-		validateCoordinates(value.coordinates, ValidatorError);
-		return value;
+		if (!Array.isArray(coordinates)) {
+			throw new ValidatorError(`Property "coordinates" must be an array`);
+		}
+		if (coordinates.length !== 2) {
+			throw new ValidatorError(`Property "coordinates" should contain exactly two items`);
+		}
+		if (typeof coordinates[0] !== "number" || typeof coordinates[1] !== "number") {
+			throw new ValidatorError("Both coordinates must be numbers");
+		}
+		if (coordinates[0] > 180 || coordinates[0] < -180) {
+			throw new ValidatorError("Longitude must be within the range -180 to 180");
+		}
+		if (coordinates[1] > 90 || coordinates[1] < -90) {
+			throw new ValidatorError("Latitude must be within the range -90 to 90");
+		}
 	}
 }
 
