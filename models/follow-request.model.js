e53692456b263b7b28691b95ef7b7a6d29b73def
FlameWolf <ajifocus@LIVE.COM>
2022-03-13 22:49:40 +0530

    Add follow request schema

---
+++ models/follow-request.model.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const { ObjectId } = require("bson");
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
+
+const followRequestSchema = new mongoose.Schema(
+	{
+		user: { type: ObjectId, ref: "User", required: true, index: true },
+		requestedBy: { type: ObjectId, ref: "User", required: true }
+	},
+	{
+		collation: {
+			locale: "en",
+			strength: 2
+		}
+	}
+);
+followRequestSchema.index({ user: 1, requestedBy: 1 }, { unique: true });
+followRequestSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model("FollowRequest", followRequestSchema);
