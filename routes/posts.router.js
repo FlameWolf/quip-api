3efed8b1ada92d9eb750636b965cc92bae418ee2
FlameWolf <ajifocus@LIVE.COM>
2022-03-17 15:22:10 +0530

    Update posts.router.js

--- routes/posts.router.js
+++ routes/posts.router.js
@@ -8,14 +8,14 @@ const multerController = require("../controllers/multer.controller");
 const favouritesController = require("../controllers/favourites.controller");
 const mutesController = require("../controllers/mutes.controller");
 
-router.post("/create", authenticateRequest, multerController.extractMediaFile.single("media-attachment"), postsController.createPost);
+router.post("/create", authenticateRequest, multerController.extractMediaFile.single("media"), postsController.createPost);
 router.get("/:postId", postsController.getPost);
 router.get("/favourite/:postId", authenticateRequest, favouritesController.addFavourite);
 router.get("/unfavourite/:postId", authenticateRequest, favouritesController.removeFavourite);
-router.post("/quote/:postId", authenticateRequest, postsController.quotePost);
+router.post("/quote/:postId", authenticateRequest, multerController.extractMediaFile.single("media"), postsController.quotePost);
 router.get("/repeat/:postId", authenticateRequest, postsController.repeatPost);
 router.get("/unrepeat/:postId", authenticateRequest, postsController.unrepeatPost);
-router.post("/reply/:postId", authenticateRequest, multerController.extractMediaFile.single("media-attachment"), postsController.replyToPost);
+router.post("/reply/:postId", authenticateRequest, multerController.extractMediaFile.single("media"), postsController.replyToPost);
 router.get("/mute/:postId", authenticateRequest, mutesController.mutePost);
 router.get("/unmute/:postId", authenticateRequest, mutesController.unmutePost);
 router.get("/delete/:postId", authenticateRequest, postsController.deletePost);
