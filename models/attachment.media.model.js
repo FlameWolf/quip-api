bc511773f362d98260aee9e4134c9da8ea420eaa
FlameWolf <ajifocus@LIVE.COM>
2022-03-17 15:14:44 +0530

    Update attachment.media.model.js

--- models/attachment.media.model.js
+++ models/attachment.media.model.js
@@ -3,15 +3,14 @@
 const mongoose = require("mongoose");
 
 const attachmentMediaSchema = new mongoose.Schema({
-	type: new mongoose.Schema({
-		type: {
-			type: String,
-			enum: ["image", "video"],
-			required: true
-		},
-		src: { type: mongoose.SchemaTypes.Url, required: true },
-		previewSrc: { type: mongoose.SchemaTypes.Url }
-	})
+	type: {
+		type: String,
+		enum: ["image", "video"],
+		required: true
+	},
+	src: { type: mongoose.SchemaTypes.Url, required: true },
+	previewSrc: { type: mongoose.SchemaTypes.Url },
+	description: { type: String }
 });
 
 module.exports = mongoose.model("AttachmentMedia", attachmentMediaSchema);
