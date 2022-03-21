f4dafbc3404d0e9acd4699b07ea9bc89305cfb09
FlameWolf <ajifocus@LIVE.COM>
2022-03-21 22:06:11 +0530

    Add request authentication for timeline route

--- routes/index.router.js
+++ routes/index.router.js
@@ -2,10 +2,11 @@
 
 const express = require("express");
 const router = express.Router();
+const authenticateRequest = require("../middleware/authenticate-request");
 const indexController = require("../controllers/index.controller");
 
 router.get("/", indexController.home);
-router.get("/timeline", indexController.timeline);
+router.get("/timeline", authenticateRequest, indexController.timeline);
 router.get("/timeline/top", indexController.timelineTop);
 
 module.exports = router;
