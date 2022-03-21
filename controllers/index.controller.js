14adc79e36638b7ff610884fc15e778c29e385b8
FlameWolf <ajifocus@LIVE.COM>
2022-03-21 22:06:28 +0530

    Implement timeline loading

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const timelineAggregationPipeline = require("../aggregations/timeline");
 const User = require("../models/user.model");
 const Post = require("../models/post.model");
 
@@ -7,7 +8,14 @@ const home = async (req, res, next) => {
 	res.status(200).end();
 };
 const timeline = async (req, res, next) => {
-	res.status(200).end();
+	const userId = req.userInfo.userId;
+	const lastPostId = req.query.lastPostId;
+	try {
+		const posts = await Post.aggregate(timelineAggregationPipeline(userId, lastPostId));
+		res.status(200).json({ posts });
+	} catch (err) {
+		res.status(500).send(err);
+	}
 };
 const timelineTop = async (req, res, next) => {
 	res.status(200).end();
