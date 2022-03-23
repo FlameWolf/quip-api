a6917d47c3cf28f22a1fec98526f9ce66efcfb88
FlameWolf <ajifocus@LIVE.COM>
2022-03-23 14:52:31 +0530

    Refactor code

--- db/aggregations/topmost.js
+++ db/aggregations/topmost.js
@@ -1,34 +1,36 @@
 const { ObjectId } = require("bson");
 
-const topmostAggregationPipeline = (userId, period = "day", lastPostId = undefined) => {
+const topmostAggregationPipeline = (userId, period = "", lastPostId = undefined) => {
+	const now = new Date();
+	let maxDate = undefined;
+	switch (period.toLowerCase()) {
+		case "all":
+			break;
+		case "year":
+			maxDate = new Date(now.setFullYear(now.getFullYear() - 1));
+			break;
+		case "month":
+			maxDate = new Date(now.setMonth(now.getMonth() - 1));
+			break;
+		case "week":
+			maxDate = new Date(now.setDate(now.getDate() - 7));
+			break;
+		case "day":
+		default:
+			maxDate = new Date(now.setDate(now.getDate() - 1));
+			break;
+	}
 	return [
 		{
-			$lookup: {
-				from: "users",
-				pipeline: [
-					{
-						$match: {
-							_id: ObjectId(userId)
-						}
-					},
-					{
-						$project: {
-							_id: 1
-						}
-					}
-				],
-				as: "user"
-			}
-		},
-		{
-			$addFields: {
-				userId: {
-					$arrayElemAt: ["$user._id", 0]
+			$match: maxDate ? {
+				createdAt: {
+					$gt: maxDate
+				}
+			} : {
+				$expr: {
+					$eq: [1, 1]
 				}
 			}
-		},
-		{
-			$unset: "user"
 		},
 		{
 			$lookup: {
@@ -54,222 +56,252 @@ const topmostAggregationPipeline = (userId, period = "day", lastPostId = undefin
 		{
 			$unwind: "$author"
 		},
-		{
-			$lookup: {
-				from: "blocks",
-				localField: "userId",
-				foreignField: "blockedBy",
-				pipeline: [
-					{
-						$group: {
-							_id: undefined,
-							result: {
-								$addToSet: "$user"
+		...(userId ? [
+			{
+				$lookup: {
+					from: "users",
+					pipeline: [
+						{
+							$match: {
+								_id: ObjectId(userId)
+							}
+						},
+						{
+							$project: {
+								_id: 1
 							}
 						}
+					],
+					as: "user"
+				}
+			},
+			{
+				$addFields: {
+					userId: {
+						$arrayElemAt: ["$user._id", 0]
 					}
-				],
-				as: "blockedUsers"
-			}
-		},
-		{
-			$addFields: {
-				blockedUsers: {
-					$ifNull: [
+				}
+			},
+			{
+				$unset: "user"
+			},
+			{
+				$lookup: {
+					from: "blocks",
+					localField: "userId",
+					foreignField: "blockedBy",
+					pipeline: [
 						{
-							$arrayElemAt: ["$blockedUsers.result", 0]
-						},
-						[]
-					]
-				}
-			}
-		},
-		{
-			$match: {
-				$expr: {
-					$not: {
-						$in: ["$author._id", "$blockedUsers"]
-					}
-				}
-			}
-		},
-		{
-			$unset: "blockedUsers"
-		},
-		{
-			$lookup: {
-				from: "mutedusers",
-				localField: "userId",
-				foreignField: "mutedBy",
-				pipeline: [
-					{
-						$group: {
-							_id: undefined,
-							result: {
-								$addToSet: "$user"
-							}
-						}
-					}
-				],
-				as: "mutedUsers"
-			}
-		},
-		{
-			$addFields: {
-				mutedUsers: {
-					$ifNull: [
-						{
-							$arrayElemAt: ["$mutedUsers.result", 0]
-						},
-						[]
-					]
-				}
-			}
-		},
-		{
-			$match: {
-				$expr: {
-					$not: {
-						$in: ["$author._id", "$mutedUsers"]
-					}
-				}
-			}
-		},
-		{
-			$unset: "mutedUsers"
-		},
-		{
-			$lookup: {
-				from: "mutedposts",
-				localField: "userId",
-				foreignField: "mutedBy",
-				pipeline: [
-					{
-						$group: {
-							_id: undefined,
-							result: {
-								$addToSet: "$post"
-							}
-						}
-					}
-				],
-				as: "mutedPosts"
-			}
-		},
-		{
-			$addFields: {
-				mutedPosts: {
-					$ifNull: [
-						{
-							$arrayElemAt: ["$mutedPosts.result", 0]
-						},
-						[]
-					]
-				}
-			}
-		},
-		{
-			$match: {
-				$expr: {
-					$not: {
-						$in: ["$_id", "$mutedPosts"]
-					}
-				}
-			}
-		},
-		{
-			$unset: "mutedPosts"
-		},
-		{
-			$lookup: {
-				from: "mutedwords",
-				localField: "userId",
-				foreignField: "mutedBy",
-				pipeline: [
-					{
-						$project: {
-							_id: 0,
-							regEx: {
-								$switch: {
-									branches: [
-										{
-											case: {
-												$eq: ["$match", "startsWith"]
-											},
-											then: {
-												$concat: ["\\W+", "$word", ".*"]
-											}
-										},
-										{
-											case: {
-												$eq: ["$match", "endsWith"]
-											},
-											then: {
-												$concat: ["w*", "$word", "(\\W+|$)"]
-											}
-										},
-										{
-											case: {
-												$eq: ["$match", "exact"]
-											},
-											then: {
-												$concat: ["\\W+", "$word", "(\\W+|$)"]
-											}
-										}
-									],
-									default: "$word"
+							$group: {
+								_id: undefined,
+								result: {
+									$addToSet: "$user"
 								}
 							}
 						}
-					},
-					{
-						$group: {
-							_id: undefined,
-							result: {
-								$addToSet: "$regEx"
-							}
+					],
+					as: "blockedUsers"
+				}
+			},
+			{
+				$addFields: {
+					blockedUsers: {
+						$ifNull: [
+							{
+								$arrayElemAt: ["$blockedUsers.result", 0]
+							},
+							[]
+						]
+					}
+				}
+			},
+			{
+				$match: {
+					$expr: {
+						$not: {
+							$in: ["$author._id", "$blockedUsers"]
 						}
 					}
-				],
-				as: "mutedWords"
-			}
-		},
-		{
-			$addFields: {
-				mutedWords: {
-					$ifNull: [
-						{
-							$arrayElemAt: ["$mutedWords.result", 0]
-						},
-						[]
-					]
 				}
-			}
-		},
-		{
-			$match: {
-				$expr: {
-					$eq: [
+			},
+			{
+				$unset: "blockedUsers"
+			},
+			{
+				$lookup: {
+					from: "mutedusers",
+					localField: "userId",
+					foreignField: "mutedBy",
+					pipeline: [
 						{
-							$filter: {
-								input: "$mutedWords",
-								cond: {
-									$regexMatch: {
-										input: "$content",
-										regex: "$$this",
-										options: "i"
+							$group: {
+								_id: undefined,
+								result: {
+									$addToSet: "$user"
+								}
+							}
+						}
+					],
+					as: "mutedUsers"
+				}
+			},
+			{
+				$addFields: {
+					mutedUsers: {
+						$ifNull: [
+							{
+								$arrayElemAt: ["$mutedUsers.result", 0]
+							},
+							[]
+						]
+					}
+				}
+			},
+			{
+				$match: {
+					$expr: {
+						$not: {
+							$in: ["$author._id", "$mutedUsers"]
+						}
+					}
+				}
+			},
+			{
+				$unset: "mutedUsers"
+			},
+			{
+				$lookup: {
+					from: "mutedposts",
+					localField: "userId",
+					foreignField: "mutedBy",
+					pipeline: [
+						{
+							$group: {
+								_id: undefined,
+								result: {
+									$addToSet: "$post"
+								}
+							}
+						}
+					],
+					as: "mutedPosts"
+				}
+			},
+			{
+				$addFields: {
+					mutedPosts: {
+						$ifNull: [
+							{
+								$arrayElemAt: ["$mutedPosts.result", 0]
+							},
+							[]
+						]
+					}
+				}
+			},
+			{
+				$match: {
+					$expr: {
+						$not: {
+							$in: ["$_id", "$mutedPosts"]
+						}
+					}
+				}
+			},
+			{
+				$unset: "mutedPosts"
+			},
+			{
+				$lookup: {
+					from: "mutedwords",
+					localField: "userId",
+					foreignField: "mutedBy",
+					pipeline: [
+						{
+							$project: {
+								_id: 0,
+								regEx: {
+									$switch: {
+										branches: [
+											{
+												case: {
+													$eq: ["$match", "startsWith"]
+												},
+												then: {
+													$concat: ["\\W+", "$word", ".*"]
+												}
+											},
+											{
+												case: {
+													$eq: ["$match", "endsWith"]
+												},
+												then: {
+													$concat: ["w*", "$word", "(\\W+|$)"]
+												}
+											},
+											{
+												case: {
+													$eq: ["$match", "exact"]
+												},
+												then: {
+													$concat: ["\\W+", "$word", "(\\W+|$)"]
+												}
+											}
+										],
+										default: "$word"
 									}
 								}
 							}
 						},
-						[]
-					]
+						{
+							$group: {
+								_id: undefined,
+								result: {
+									$addToSet: "$regEx"
+								}
+							}
+						}
+					],
+					as: "mutedWords"
 				}
+			},
+			{
+				$addFields: {
+					mutedWords: {
+						$ifNull: [
+							{
+								$arrayElemAt: ["$mutedWords.result", 0]
+							},
+							[]
+						]
+					}
+				}
+			},
+			{
+				$match: {
+					$expr: {
+						$eq: [
+							{
+								$filter: {
+									input: "$mutedWords",
+									cond: {
+										$regexMatch: {
+											input: "$content",
+											regex: "$$this",
+											options: "i"
+										}
+									}
+								}
+							},
+							[]
+						]
+					}
+				}
+			},
+			{
+				$unset: ["mutedWords", "userId"]
 			}
-		},
-		{
-			$unset: ["mutedWords", "userId"]
-		},
+		] : []),
 		{
 			$lookup: {
 				from: "favourites",
@@ -391,17 +423,15 @@ const topmostAggregationPipeline = (userId, period = "day", lastPostId = undefin
 			$unset: ["favouriteCount", "replyCount", "repeatCount"]
 		},
 		{
-			$match: lastPostId
-				? {
-					_id: {
-						$lt: ObjectId(lastPostId)
-					}
+			$match: lastPostId ? {
+				_id: {
+					$lt: ObjectId(lastPostId)
 				}
-				: {
-					$expr: {
-						$eq: [1, 1]
-					}
+			} : {
+				$expr: {
+					$eq: [1, 1]
 				}
+			}
 		},
 		{
 			$limit: 20
