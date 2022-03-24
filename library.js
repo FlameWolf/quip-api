1041145fdd1e75a3ce317027bc9cf711dbc47670
FlameWolf <ajifocus@LIVE.COM>
2022-03-24 15:29:25 +0530

    Add URL and email regular expressions

--- library.js
+++ library.js
@@ -3,6 +3,8 @@
 const invalidHandles = ["auth", "home", "search", "user", "users", "post", "posts", "quip", "quips", "favourite", "favourites", "unfavourite", "repeat", "repeats", "unrepeat", "reply", "replies", "profile", "profiles", "setting", "settings", "follow", "followed", "follows", "following", "follower", "followers", "unfollow", "mute", "muted", "unmute", "block", "blocked", "unblock", "filter", "filters", "list", "lists", "bookmark", "bookmarks", "unbookmark", "hashtag", "hashtags", "notification", "notifications", "message", "messages", "account", "accounts", "security", "privacy", "admin"];
 const handleRegExp = /^[A-Za-z][\w]{3,31}$/;
 const passwordRegExp = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+const urlRegExp = /^(\w+[~@#$\-_+]?\w+:\/\/)((\w)+[\-\._~:\/\?#\[\]@!$&'\(\)\*+,;=%]?)+$/;
+const emailRegExp = /^[\w\.-]+@[\w\.-]+\.\w+$/;
 const rounds = 10;
 const authTokenLife = 1000 * 3600 * 24 * 7;
 const refreshTokenLife = 1000 * 3600 * 24 * 360;
@@ -15,6 +17,8 @@ module.exports = {
 	invalidHandles,
 	handleRegExp,
 	passwordRegExp,
+	urlRegExp,
+	emailRegExp,
 	rounds,
 	authTokenLife,
 	refreshTokenLife,
