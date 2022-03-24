b06cae151067717f50ddd8fda52a91626e0d5b73
FlameWolf <ajifocus@LIVE.COM>
2022-03-24 19:31:21 +0530

    Update mention.model.js

--- models/mention.model.js
+++ models/mention.model.js
@@ -2,22 +2,10 @@
 
 const { ObjectId } = require("bson");
 const mongoose = require("mongoose");
-const { maxMentionsCount } = require("../library");
 
 const mentionSchema = new mongoose.Schema({
 	post: { type: ObjectId, ref: "Post" },
-	mentions: {
-		type: [
-			{
-				type: ObjectId,
-				ref: "User"
-			}
-		],
-		validate: {
-			validator: value => value.length <= maxMentionsCount,
-			message: "Maximum number of mentions exceeded"
-		}
-	}
+	mentionedUser: { type: ObjectId, ref: "User" }
 });
 
 module.exports = mongoose.model("Mention", mentionSchema);
