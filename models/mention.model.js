67a39cb66c660a3ade47af3ad218f6fb06978f67
FlameWolf <ajifocus@LIVE.COM>
2022-03-24 19:37:22 +0530

    Add indexes

--- models/mention.model.js
+++ models/mention.model.js
@@ -4,8 +4,8 @@ const { ObjectId } = require("bson");
 const mongoose = require("mongoose");
 
 const mentionSchema = new mongoose.Schema({
-	post: { type: ObjectId, ref: "Post" },
-	mentionedUser: { type: ObjectId, ref: "User" }
+	post: { type: ObjectId, ref: "Post", index: true },
+	mentionedUser: { type: ObjectId, ref: "User", index: true }
 });
 
 module.exports = mongoose.model("Mention", mentionSchema);
