85b6b23035add67d288ca401f96b6421dc7f4f5f
FlameWolf <ajifocus@LIVE.COM>
2022-03-26 00:03:38 +0530

    Add `ping` route

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -2,10 +2,12 @@
 
 const express = require("express");
 const router = express.Router();
+const authenticateRequest = require("../middleware/authenticate-request");
 const authController = require("../controllers/auth.controller");
 
 router.post("/sign-up", authController.signUp);
 router.post("/sign-in", authController.signIn);
+router.get("/ping", authenticateRequest, authController.ping);
 router.get("/refresh-auth-token", authController.refreshAuthToken);
 router.get("/sign-out", authController.signOut);
 
