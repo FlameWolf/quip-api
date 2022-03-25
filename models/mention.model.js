4fa08f67d4123567eb4d77d4c7d68e102f9b1d8e
FlameWolf <ajifocus@LIVE.COM>
2022-03-25 17:34:41 +0530

    Rename `mentionedUser` to `mentioned`; add timestamps

--- models/mention.model.js
+++ models/mention.model.js
@@ -3,9 +3,14 @@
 const { ObjectId } = require("bson");
 const mongoose = require("mongoose");
 
-const mentionSchema = new mongoose.Schema({
-	post: { type: ObjectId, ref: "Post", index: true },
-	mentionedUser: { type: ObjectId, ref: "User", index: true }
-});
+const mentionSchema = new mongoose.Schema(
+	{
+		post: { type: ObjectId, ref: "Post", index: true },
+		mentioned: { type: ObjectId, ref: "User", index: true }
+	},
+	{
+		timestamps: true
+	}
+);
 
 module.exports = mongoose.model("Mention", mentionSchema);
