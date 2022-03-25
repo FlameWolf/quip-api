29a81fdb5f0d6f15bad116922c709f0743366506
FlameWolf <ajifocus@LIVE.COM>
2022-03-25 19:55:43 +0530

    Add missing token error message

--- middleware/authenticate-request.js
+++ middleware/authenticate-request.js
@@ -5,6 +5,9 @@ const jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
 	try {
 		const authToken = req.headers.authorization?.replace(/^bearer\s+/i, "");
+		if(!authToken) {
+			throw new Error("Atuhentication token not found");
+		}
 		req.userInfo = jwt.verify(authToken, process.env.JWT_AUTH_SECRET);
 	} catch (err) {
 		res.status(401).send(err);
