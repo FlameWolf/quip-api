965085dbc0e3e07241bb27258d53258353fd2142
FlameWolf <ajifocus@LIVE.COM>
2022-03-25 17:36:35 +0530

    Add text index for `description`; add timestamps; specify default collation

--- models/media-file.model.js
+++ models/media-file.model.js
@@ -2,15 +2,35 @@
 
 const mongoose = require("mongoose");
 
-const mediaFileSchema = new mongoose.Schema({
-	fileType: {
-		type: String,
-		enum: ["image", "video"],
-		required: true
+const mediaFileSchema = new mongoose.Schema(
+	{
+		fileType: {
+			type: String,
+			enum: ["image", "video"],
+			required: true
+		},
+		src: { type: mongoose.SchemaTypes.Url, required: true },
+		previewSrc: { type: mongoose.SchemaTypes.Url },
+		description: {
+			type: String,
+			text: true,
+			index: {
+				partialFilterExpression: {
+					description: {
+						$exists: true,
+						$ne: null
+					}
+				}
+			}
+		}
 	},
-	src: { type: mongoose.SchemaTypes.Url, required: true },
-	previewSrc: { type: mongoose.SchemaTypes.Url },
-	description: { type: String }
-});
+	{
+		timestamps: true,
+		collation: {
+			locale: "en",
+			strength: 2
+		}
+	}
+);
 
 module.exports = mongoose.model("MediaFile", mediaFileSchema);
