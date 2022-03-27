eee727caea938403d49aef58d9f6d1b9362b5760
FlameWolf <ajifocus@LIVE.COM>
2022-03-27 12:07:28 +0530

    Remove `ping` route

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -2,12 +2,10 @@
 
 const express = require("express");
 const router = express.Router();
-const authenticateRequest = require("../middleware/authenticate-request");
 const authController = require("../controllers/auth.controller");
 
 router.post("/sign-up", authController.signUp);
 router.post("/sign-in", authController.signIn);
-router.get("/ping", authenticateRequest, authController.ping);
 router.get("/refresh-auth-token", authController.refreshAuthToken);
 router.get("/sign-out", authController.signOut);
 
