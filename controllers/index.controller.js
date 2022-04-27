465b0b99bc58692a1217299aeb18634c5300dd15
FlameWolf <agnijith@gmail.com>
2022-04-27 17:30:17 +0530

    Add `activity` method

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const timelineAggregationPipeline = require("../db/pipelines/timeline");
+const activityAggregationPipeline = require("../db/pipelines/activity");
 const topmostAggregationPipeline = require("../db/pipelines/topmost");
 const User = require("../models/user.model");
 const Post = require("../models/post.model");
@@ -10,6 +11,17 @@ const timeline = async (req, res, next) => {
 	const { includeRepeats, includeReplies, lastPostId } = req.query;
 	try {
 		const posts = await User.aggregate(timelineAggregationPipeline(userId, includeRepeats !== "false", includeReplies !== "false", lastPostId));
+		res.status(200).json({ posts });
+	} catch (err) {
+		res.status(500).send(err);
+	}
+};
+const activity = async (req, res, next) => {
+	const userId = req.userInfo?.userId;
+	const period = req.params.period;
+	const lastPostId = req.query.lastPostId;
+	try {
+		const posts = await User.aggregate(activityAggregationPipeline(userId, period, lastPostId));
 		res.status(200).json({ posts });
 	} catch (err) {
 		res.status(500).send(err);
@@ -27,4 +39,8 @@ const topmost = async (req, res, next) => {
 	}
 };
 
-module.exports = { timeline, topmost };
+module.exports = {
+	timeline,
+	activity,
+	topmost
+};
