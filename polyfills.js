6bf0d4dd1c2dda8f7c8cfc5e7b31216aee9a966f
FlameWolf <agnijith@gmail.com>
2022-04-27 22:12:26 +0530

    Fix `Array.prototype.at` method logic; Add `setProperty` and `getProperty` methods to `Object.prototype`

--- polyfills.js
+++ polyfills.js
@@ -1,5 +1,31 @@
+"use strict";
+
 if (!Array.prototype.at) {
 	Array.prototype.at = function (index) {
-		return this[this.length + index];
+		return this[index < 0 ? this.length + index : index];
 	};
+}
+if (!Object.prototype.setProperty) {
+	const setProperty = (operand, path, value) => {
+		const segments = Array.isArray(path) ? path : path.split(".");
+		operand = operand || {};
+		if (segments.length > 1) {
+			const segment = segments.shift();
+			operand[segment] = operand[segment] || {};
+			setProperty(operand[segment], segments, value);
+		} else {
+			operand[segments.pop()] = value;
+		}
+	};
+	Object.prototype.setProperty = setProperty;
+}
+if (!Object.prototype.getProperty) {
+	const getProperty = (operand, path) => {
+		const segments = Array.isArray(path) ? path : path.split(".");
+		if (operand && segments.length) {
+			return getProperty(operand[segments.shift()], segments);
+		}
+		return operand;
+	};
+	Object.prototype.getProperty = getProperty;
 }
