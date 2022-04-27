143da62dc00b90b73e12053f26a94ffe5826117a
FlameWolf <agnijith@gmail.com>
2022-04-27 19:34:25 +0530

    Create settings.model.js

---
+++ models/settings.model.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const { ObjectId } = require("bson");
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+const settingsSchema = new Schema(
+	{
+		user: { type: ObjectId, ref: "User", index: true },
+		timeline: new Schema({
+			includeRepeats: { type: Boolean, default: true },
+			includeReplies: { type: Boolean, default: true }
+		}),
+		activity: new Schema({
+			period: {
+				type: String,
+				enum: ["day", "week", "month"],
+				default: "day"
+			}
+		}),
+		topmost: new Schema({
+			period: {
+				type: String,
+				enum: ["day", "week", "month", "year", "all"],
+				default: "day"
+			}
+		}),
+		profile: new Schema({
+			includeRepeats: { type: Boolean, default: false },
+			includeReplies: { type: Boolean, default: false }
+		})
+	},
+	{
+		timestamps: true,
+		collation: {
+			locale: "en",
+			strength: 2
+		}
+	}
+);
+
+module.exports = mongoose.model("Settings", settingsSchema);
