616d5090dc357e6725da70d1ecaa8c560406ca39
FlameWolf <agnijith@gmail.com>
2022-04-27 19:51:46 +0530

    Enable strict mode in multer controller files

--- controllers/multer-cloud-storage.controller.js
+++ controllers/multer-cloud-storage.controller.js
@@ -1,6 +1,7 @@
+"use strict";
+
 const multer = require("multer");
 const multerCloudStorage = require("multer-cloud-storage");
-const path = require("path");
 const validMimeTypes = ["image", "video"];
 const { megaByte } = require("../library");
 
