bf5b13d2eff2e4ff2d0d452e1a097e9a3e6a5e28
FlameWolf <agnijith@gmail.com>
2022-04-28 16:04:15 +0530

    Specify default collation

--- models/settings.model.js
+++ models/settings.model.js
@@ -4,30 +4,38 @@ const { ObjectId } = require("bson");
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
-const settingsSchema = new Schema({
-	user: { type: ObjectId, ref: "User", index: true },
-	timeline: new Schema({
-		includeRepeats: { type: Boolean, default: true },
-		includeReplies: { type: Boolean, default: true }
-	}),
-	activity: new Schema({
-		period: {
-			type: String,
-			enum: ["day", "week", "month"],
-			default: "day"
+const settingsSchema = new Schema(
+	{
+		user: { type: ObjectId, ref: "User", index: true },
+		timeline: new Schema({
+			includeRepeats: { type: Boolean, default: true },
+			includeReplies: { type: Boolean, default: true }
+		}),
+		activity: new Schema({
+			period: {
+				type: String,
+				enum: ["day", "week", "month"],
+				default: "day"
+			}
+		}),
+		topmost: new Schema({
+			period: {
+				type: String,
+				enum: ["day", "week", "month", "year", "all"],
+				default: "day"
+			}
+		}),
+		profile: new Schema({
+			includeRepeats: { type: Boolean, default: false },
+			includeReplies: { type: Boolean, default: false }
+		})
+	},
+	{
+		collation: {
+			locale: "en",
+			strength: 2
 		}
-	}),
-	topmost: new Schema({
-		period: {
-			type: String,
-			enum: ["day", "week", "month", "year", "all"],
-			default: "day"
-		}
-	}),
-	profile: new Schema({
-		includeRepeats: { type: Boolean, default: false },
-		includeReplies: { type: Boolean, default: false }
-	})
-});
+	}
+);
 
 module.exports = mongoose.model("Settings", settingsSchema);
