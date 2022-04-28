8fdc6edf0506e6db510a68fac8dc2d076eddfd54
FlameWolf <agnijith@gmail.com>
2022-04-28 15:14:55 +0530

    Map settings controller methods to routes

--- routes/settings.router.js
+++ routes/settings.router.js
@@ -3,16 +3,21 @@
 const express = require("express");
 const router = express.Router();
 const authenticateRequest = require("../middleware/authenticate-request");
+const settingsController = require("../controllers/settings.controller");
 const mutesController = require("../controllers/mutes.controller");
 const usersController = require("../controllers/users.controller");
 const followRequestsController = require("../controllers/follow-requests.controller");
 
+router.post("/", authenticateRequest, settingsController.updateSettings);
+router.put("/:path", authenticateRequest, settingsController.updateSettingByPath);
+router.get("/", authenticateRequest, settingsController.getSettings);
+router.get("/:path", authenticateRequest, settingsController.getSettingByPath);
 router.post("/mute", authenticateRequest, mutesController.muteWord);
 router.post("/unmute", authenticateRequest, mutesController.unmuteWord);
-router.get("/accept_req/{requestId}", authenticateRequest, followRequestsController.acceptFollowRequest);
+router.get("/accept_req/:requestId", authenticateRequest, followRequestsController.acceptFollowRequest);
 router.post("/accept_reqs", authenticateRequest, followRequestsController.acceptSelectedFollowRequests);
 router.get("/accept_all_reqs", authenticateRequest, followRequestsController.acceptAllFollowRequests);
-router.get("/reject_req/{requestId}", authenticateRequest, followRequestsController.rejectFollowRequest);
+router.get("/reject_req/:requestId", authenticateRequest, followRequestsController.rejectFollowRequest);
 router.post("/reject_reqs", authenticateRequest, followRequestsController.rejectSelectedFollowRequests);
 router.get("/reject_all_reqs", authenticateRequest, followRequestsController.rejectAllFollowRequests);
 router.get("/deactivate", authenticateRequest, usersController.deactivateUser);
