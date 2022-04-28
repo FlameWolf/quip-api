ac94a2d0c1da09471db138517067d7ded77b23e3
FlameWolf <agnijith@gmail.com>
2022-04-28 12:46:26 +0530

    Add `setProperty` and `getProperty` methods

--- library.js
+++ library.js
@@ -12,6 +12,25 @@ const maxContentLength = 256;
 const maxMutedWordLength = 256;
 const megaByte = 1024 * 1024;
 
+const setProperty = (operand, path, value) => {
+	const segments = Array.isArray(path) ? path : path.split(".");
+	operand = operand || {};
+	if (segments.length > 1) {
+		const segment = segments.shift();
+		operand[segment] = operand[segment] || {};
+		setProperty(operand[segment], segments, value);
+	} else {
+		operand[segments.pop()] = value;
+	}
+};
+const getProperty = (operand, path) => {
+	const segments = Array.isArray(path) ? path : path.split(".");
+	if (operand && segments.length) {
+		return getProperty(operand[segments.shift()], segments);
+	}
+	return operand;
+};
+
 module.exports = {
 	invalidHandles,
 	handleRegExp,
@@ -23,5 +42,7 @@ module.exports = {
 	contentLengthRegExp,
 	maxContentLength,
 	maxMutedWordLength,
-	megaByte
+	megaByte,
+	setProperty,
+	getProperty
 };
