a63c390476b24e198dbfa1c45c4f362e3697ab5a
FlameWolf <agnijith@gmail.com>
2022-04-28 12:48:37 +0530

    Undo adding `setProperty` and `getProperty` methods to `Object.prototype`

--- polyfills.js
+++ polyfills.js
@@ -5,27 +5,3 @@ if (!Array.prototype.at) {
 		return this[index < 0 ? this.length + index : index];
 	};
 }
-if (!Object.prototype.setProperty) {
-	const setProperty = (operand, path, value) => {
-		const segments = Array.isArray(path) ? path : path.split(".");
-		operand = operand || {};
-		if (segments.length > 1) {
-			const segment = segments.shift();
-			operand[segment] = operand[segment] || {};
-			setProperty(operand[segment], segments, value);
-		} else {
-			operand[segments.pop()] = value;
-		}
-	};
-	Object.prototype.setProperty = setProperty;
-}
-if (!Object.prototype.getProperty) {
-	const getProperty = (operand, path) => {
-		const segments = Array.isArray(path) ? path : path.split(".");
-		if (operand && segments.length) {
-			return getProperty(operand[segments.shift()], segments);
-		}
-		return operand;
-	};
-	Object.prototype.getProperty = getProperty;
-}
