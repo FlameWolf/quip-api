ae30af40fb42b9238d55fa1d93b254612824efb3
FlameWolf <agnijith@gmail.com>
2022-04-29 18:28:23 +0530

    Add `findFollowingByUserId`, `findFollowersByUserId`, `getUserFollowing`, and `getUserFollowers` methods

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -2,7 +2,10 @@
 
 const postsAggregationPipeline = require("../db/pipelines/posts");
 const favouritesAggregationPipeline = require("../db/pipelines/favourites");
+const followingAggregationPipeline = require("../db/pipelines/following");
+const followersAggregationPipeline = require("../db/pipelines/followers");
 const User = require("../models/user.model");
+const Follows = require("../models/follow.model");
 
 const findActiveUserById = async userId => await User.findOne({ _id: userId, deactivated: false, deleted: false });
 const findActiveUserByHandle = async handle => await User.findOne({ handle, deactivated: false, deleted: false });
@@ -10,6 +13,8 @@ const findUserById = async userId => await User.findOne({ _id: userId, deleted: 
 const findUserByHandle = async handle => await User.findOne({ handle, deleted: false });
 const findPostsByUserId = async (userId, includeRepeats, includeReplies, lastPostId) => await User.aggregate(postsAggregationPipeline(userId, includeRepeats, includeReplies, lastPostId));
 const findFavouritesByUserId = async (userId, lastPostId) => await User.aggregate(favouritesAggregationPipeline(userId, lastPostId));
+const findFollowingByUserId = async (userId, lastFollowId) => await Follows.aggregate(followingAggregationPipeline(userId, lastFollowId));
+const findFollowersByUserId = async (userId, lastFollowId) => await Follows.aggregate(followersAggregationPipeline(userId, lastFollowId));
 const getUser = async (req, res, next) => {
 	const handle = req.params.handle;
 	try {
@@ -53,6 +58,36 @@ const getUserFavourites = async (req, res, next) => {
 		res.status(500).send(err);
 	}
 };
+const getUserFollowing = async (req, res, next) => {
+	const handle = req.params.handle;
+	const lastFollowId = req.query.lastFollowId;
+	try {
+		const user = await findActiveUserByHandle(handle);
+		if (!user) {
+			res.status(404).send("User not found");
+			return;
+		}
+		const following = await findFollowingByUserId(user._id, lastFollowId);
+		res.status(200).json({ following });
+	} catch (err) {
+		res.status(500).send(err);
+	}
+};
+const getUserFollowers = async (req, res, next) => {
+	const handle = req.params.handle;
+	const lastFollowId = req.query.lastFollowId;
+	try {
+		const user = await findActiveUserByHandle(handle);
+		if (!user) {
+			res.status(404).send("User not found");
+			return;
+		}
+		const followers = await findFollowersByUserId(user._id, lastFollowId);
+		res.status(200).json({ followers });
+	} catch (err) {
+		res.status(500).send(err);
+	}
+};
 const deactivateUser = async (req, res, next) => {
 	const userId = req.userInfo.userId;
 	try {
@@ -88,9 +123,13 @@ module.exports = {
 	findUserByHandle,
 	findPostsByUserId,
 	findFavouritesByUserId,
+	findFollowingByUserId,
+	findFollowersByUserId,
 	getUser,
 	getUserPosts,
 	getUserFavourites,
+	getUserFollowing,
+	getUserFollowers,
 	deactivateUser,
 	activateUser,
 	deleteUser
