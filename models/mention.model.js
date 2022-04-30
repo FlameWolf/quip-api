4e52f77c072823876f531a7775daec059c97222f
FlameWolf <agnijith@gmail.com>
2022-04-30 17:21:22 +0530

    Change `mentioned` field in into an array

--- models/mention.model.js
+++ models/mention.model.js
@@ -6,7 +6,10 @@ const mongoose = require("mongoose");
 const mentionSchema = new mongoose.Schema(
 	{
 		post: { type: ObjectId, ref: "Post", index: true },
-		mentioned: { type: ObjectId, ref: "User", index: true }
+		mentioned: {
+			type: [{ type: ObjectId, ref: "User", index: true }],
+			index: true
+		}
 	},
 	{
 		timestamps: true,
