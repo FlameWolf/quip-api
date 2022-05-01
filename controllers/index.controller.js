58a8147066166c0c0ffbae0aeca9573f81092269
FlameWolf <agnijith@gmail.com>
2022-05-01 13:24:49 +0530

    Remove `lastPostId` parameter passed to `topmostAggregationPipeline`

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -30,9 +30,8 @@ const activity = async (req, res, next) => {
 const topmost = async (req, res, next) => {
 	const userId = req.userInfo?.userId;
 	const period = req.params.period;
-	const lastPostId = req.query.lastPostId;
 	try {
-		const posts = await Post.aggregate(topmostAggregationPipeline(userId, period, lastPostId));
+		const posts = await Post.aggregate(topmostAggregationPipeline(userId, period));
 		res.status(200).json({ posts });
 	} catch (err) {
 		res.status(500).send(err);
