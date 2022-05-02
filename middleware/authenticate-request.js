50daf0305a4eca06083cc4059ea4aaa1b3a06fd2
FlameWolf <agnijith@gmail.com>
2022-05-02 21:04:53 +0530

    Invoke next middleware even if authentication fails

--- middleware/authenticate-request.js
+++ middleware/authenticate-request.js
@@ -5,13 +5,7 @@ const jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
 	try {
 		const authToken = req.headers.authorization?.replace(/^bearer\s+/i, "");
-		if(!authToken) {
-			throw new Error("Atuhentication token not found");
-		}
-		req.userInfo = jwt.verify(authToken, process.env.JWT_AUTH_SECRET);
-	} catch (err) {
-		res.status(401).send(err);
-		return;
-	}
+		req.userInfo = authToken && jwt.verify(authToken, process.env.JWT_AUTH_SECRET);
+	} catch (err) {}
 	next();
 };
