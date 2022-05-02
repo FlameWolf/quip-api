3f2fd7b76aa05eef132594f07d6eb500724845a5
FlameWolf <agnijith@gmail.com>
2022-05-02 13:11:57 +0530

    Create muted-posts.js

---
+++ db/pipelines/muted-posts.js
@@ -0,0 +1,49 @@
+"use strict";
+
+const { ObjectId } = require("bson");
+const postAggregationPipeline = require("./post");
+const interactionsAggregationPipeline = require("./interactions");
+
+const mutedPostsAggregationPipeline = (userId, lastMuteId) => [
+	{
+		$match: {
+			mutedBy: ObjectId(userId)
+		}
+	},
+	{
+		$lookup: {
+			from: "posts",
+			localField: "post",
+			foreignField: "_id",
+			pipeline: postAggregationPipeline(),
+			as: "post"
+		}
+	},
+	{
+		$unwind: "$post"
+	},
+	{
+		$sort: {
+			createdAt: -1
+		}
+	},
+	{
+		$lookup: {
+			from: "posts",
+			localField: "post._id",
+			foreignField: "_id",
+			pipeline: interactionsAggregationPipeline(userId),
+			as: "post"
+		}
+	},
+	{
+		$unwind: "post"
+	},
+	{
+		$project: {
+			post: 1
+		}
+	}
+];
+
+module.exports = mutedPostsAggregationPipeline;
