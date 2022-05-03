99abda9dae354a186c0688ed39729cff97f63dda
FlameWolf <agnijith@gmail.com>
2022-05-03 11:31:16 +0530

    Add unique constraint for `user` field

--- models/settings.model.js
+++ models/settings.model.js
@@ -2,11 +2,12 @@
 
 const { ObjectId } = require("bson");
 const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
 
 const Schema = mongoose.Schema;
 const settingsSchema = new Schema(
 	{
-		user: { type: ObjectId, ref: "User", index: true },
+		user: { type: ObjectId, ref: "User", index: true, unique: true },
 		timeline: new Schema({
 			includeRepeats: { type: Boolean, default: true },
 			includeReplies: { type: Boolean, default: true }
@@ -47,5 +48,6 @@ const settingsSchema = new Schema(
 		}
 	}
 );
+settingsSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model("Settings", settingsSchema);
