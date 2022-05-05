9fa79cf5ca07513de19cbd6d6e0d5701ff5de68a
FlameWolf <agnijith@gmail.com>
2022-05-05 16:10:38 +0530

    Add `/bookmark/:postId` and `/unbookmark/:postId` routes

--- routes/posts.router.js
+++ routes/posts.router.js
@@ -6,12 +6,15 @@ const requireAuthentication = require("../middleware/require-authentication");
 const postsController = require("../controllers/posts.controller");
 const multerCloudStorageController = require("../controllers/multer-cloud-storage.controller");
 const favouritesController = require("../controllers/favourites.controller");
+const bookmarksController = require("../controllers/bookmarks.controller");
 const mutesController = require("../controllers/mutes.controller");
 
 router.post("/create", requireAuthentication, multerCloudStorageController.uploadMediaFileToCloud, postsController.createPost);
 router.get("/:postId", postsController.getPost);
 router.get("/favourite/:postId", requireAuthentication, favouritesController.addFavourite);
 router.get("/unfavourite/:postId", requireAuthentication, favouritesController.removeFavourite);
+router.get("/bookmark/:postId", requireAuthentication, bookmarksController.addBookmark);
+router.get("/unbookmark/:postId", requireAuthentication, bookmarksController.removeBookmark);
 router.post("/quote/:postId", requireAuthentication, multerCloudStorageController.uploadMediaFileToCloud, postsController.quotePost);
 router.get("/repeat/:postId", requireAuthentication, postsController.repeatPost);
 router.get("/unrepeat/:postId", requireAuthentication, postsController.unrepeatPost);
