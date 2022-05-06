81404137f60325b40cc4f754abace694bc667158
FlameWolf <agnijith@gmail.com>
2022-05-06 11:35:00 +0530

    Create list.model.js

---
+++ models/list.model.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const { ObjectId } = require("bson");
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
+const { handleRegExp } = require("../library");
+
+const listSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			trim: true,
+			required: true,
+			validate: {
+				validator: value => handleRegExp.test(value),
+				message: "List name is not valid"
+			}
+		},
+		owner: { type: ObjectId, ref: "User", required: true },
+		includeRepeats: { type: Boolean, default: true },
+		includeReplies: { type: Boolean, default: true }
+	},
+	{
+		timestamps: true,
+		collation: {
+			locale: "en",
+			strength: 2
+		}
+	}
+);
+listSchema.index({ owner: 1, name: 1 }, { unique: true, uniqueCaseInsensitive: true });
+listSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model("List", listSchema);
