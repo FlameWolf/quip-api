02505fb3e206b751b0d58b097f5341e58ecd88fe
FlameWolf <agnijith@gmail.com>
2022-05-06 11:35:25 +0530

    Create list-member.model.js

---
+++ models/list-member.model.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const { ObjectId } = require("bson");
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
+
+const listMemberSchema = new mongoose.Schema(
+	{
+		list: { type: ObjectId, ref: "List", required: true, index: true },
+		user: { type: ObjectId, ref: "User", required: true }
+	},
+	{
+		timestamps: true,
+		collation: {
+			locale: "en",
+			strength: 2
+		}
+	}
+);
+listMemberSchema.index({ user: 1, list: 1 }, { unique: true });
+listMemberSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model("ListMember", listMemberSchema);
