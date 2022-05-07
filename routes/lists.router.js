499b6de8bf2e693608a0b7504535b40541e20fd6
FlameWolf <agnijith@gmail.com>
2022-05-07 12:54:08 +0530

    Add `/` and `/members` routes

--- routes/lists.router.js
+++ routes/lists.router.js
@@ -2,10 +2,13 @@
 
 const express = require("express");
 const router = express.Router();
+const userController = require("../controllers/users.controller");
 const listsController = require("../controllers/lists.controller");
 
+router.get("/", userController.getLists);
 router.post("/create", listsController.createList);
 router.post("/update", listsController.updateList);
+router.get("/members", userController.getListMembers);
 router.post("/add-member", listsController.addMember);
 router.post("/remove-member", listsController.removeMember);
 router.post("/delete/:name", listsController.deleteList);
