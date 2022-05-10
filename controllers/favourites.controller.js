ae732e77248e5e55246dab1623c3d30ce0622a50
FlameWolf <agnijith@gmail.com>
2022-05-10 16:16:00 +0530

    Update post score on favourite/unfavourite

--- controllers/favourites.controller.js
+++ controllers/favourites.controller.js
@@ -1,36 +1,57 @@
 "use strict";
 
+const mongoose = require("mongoose");
 const Post = require("../models/post.model");
 const Favourite = require("../models/favourite.model");
 
 const addFavourite = async (req, res, next) => {
 	const postId = req.params.postId;
 	const userId = req.userInfo.userId;
+	const session = await mongoose.startSession();
 	try {
 		if (!(await Post.countDocuments({ _id: postId }))) {
 			res.status(404).send("Post not found");
 			return;
 		}
-		const favourited = await new Favourite({
-			post: postId,
-			favouritedBy: userId
-		}).save();
-		res.status(200).json({ favourited });
+		await session.withTransaction(async () => {
+			const favourited = await new Favourite({
+				post: postId,
+				favouritedBy: userId
+			}).save({ session });
+			await Post.findByIdAndUpdate(postId, {
+				$inc: {
+					score: 1
+				}
+			}).session(session);
+			res.status(200).json({ favourited });
+		});
 	} catch (err) {
 		next(err);
+	} finally {
+		await session.endSession();
 	}
 };
 const removeFavourite = async (req, res, next) => {
 	const postId = req.params.postId;
 	const userId = req.userInfo.userId;
+	const session = await mongoose.startSession();
 	try {
-		const unfavourited = await Favourite.findOneAndDelete({
-			post: postId,
-			favouritedBy: userId
+		await session.withTransaction(async () => {
+			const unfavourited = await Favourite.findOneAndDelete({
+				post: postId,
+				favouritedBy: userId
+			}).session(session);
+			await Post.findByIdAndUpdate(postId, {
+				$inc: {
+					score: -1
+				}
+			}).session(session);
+			res.status(200).json({ unfavourited });
 		});
-		res.status(200).json({ unfavourited });
 	} catch (err) {
 		next(err);
+	} finally {
+		await session.endSession();
 	}
 };
 
