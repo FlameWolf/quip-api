3279f45da0cf1e7539c87ca42ccdb95ce1964a0e
FlameWolf <agnijith@gmail.com>
2022-05-16 13:02:44 +0530

    Refactor code; include post expired field only if post contains a poll

--- db/pipelines/post.js
+++ db/pipelines/post.js
@@ -2,51 +2,92 @@
 
 const interactionsAggregationPipeline = require("./interactions");
 
+const authorLookupAndUnwind = [
+	{
+		$lookup: {
+			from: "users",
+			localField: "author",
+			foreignField: "_id",
+			pipeline: [
+				{
+					$project: {
+						handle: {
+							$cond: ["$deleted", "[deleted]", "$handle"]
+						}
+					}
+				}
+			],
+			as: "author"
+		}
+	},
+	{
+		$unwind: "$author"
+	}
+];
+const addPollExpiredField = {
+	$addFields: {
+		attachments: {
+			$cond: [
+				{
+					$gt: ["$attachments", null]
+				},
+				{
+					$mergeObjects: [
+						"$attachments",
+						{
+							poll: {
+								$cond: [
+									{
+										$gt: ["$attachments.poll", null]
+									},
+									{
+										$mergeObjects: [
+											"$attachments.poll",
+											{
+												expired: {
+													$gt: [
+														new Date(),
+														{
+															$add: ["$createdAt", "$attachments.poll.duration"]
+														}
+													]
+												}
+											}
+										]
+									},
+									"$$REMOVE"
+								]
+							}
+						}
+					]
+				},
+				"$$REMOVE"
+			]
+		}
+	}
+};
 const postAggregationPipeline = (userId = undefined) => {
 	return [
 		{
 			$unset: "score"
 		},
+		...authorLookupAndUnwind,
 		{
 			$lookup: {
-				from: "users",
-				localField: "author",
+				from: "posts",
+				localField: "attachments.post",
 				foreignField: "_id",
-				pipeline: [
-					{
-						$project: {
-							handle: {
-								$cond: ["$deleted", "[deleted]", "$handle"]
-							}
-						}
-					}
-				],
-				as: "author"
+				pipeline: [...authorLookupAndUnwind, addPollExpiredField],
+				as: "attachments.post"
 			}
 		},
 		{
-			$unwind: "$author"
-		},
-		{
-			$addFields: {
-				"attachments.poll.expired": {
-					$cond: [
-						{
-							$gt: ["$attachments.poll", null]
-						},
-						{
-							$gt: [
-								new Date(),
-								{
-									$add: ["$createdAt", "$attachments.poll.duration"]
-								}
-							]
-						},
-						"$$REMOVE"
-					]
-				}
+			$unwind: {
+				path: "$attachments.post",
+				preserveNullAndEmptyArrays: true
 			}
 		},
+		addPollExpiredField,
 		...interactionsAggregationPipeline(userId)
 	];
 };
