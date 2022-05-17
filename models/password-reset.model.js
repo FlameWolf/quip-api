8368ad6b1afe6b3d698388db8e5847ca602da499
FlameWolf <agnijith@gmail.com>
2022-05-17 14:16:45 +0530

    Remove `mongoose-unique-validator` plugin

--- models/password-reset.model.js
+++ models/password-reset.model.js
@@ -2,7 +2,6 @@
 
 const { ObjectId } = require("bson");
 const mongoose = require("mongoose");
-const uniqueValidator = require("mongoose-unique-validator");
 
 const passwordResetSchema = new mongoose.Schema(
 	{
@@ -21,6 +20,5 @@ const passwordResetSchema = new mongoose.Schema(
 	}
 );
 passwordResetSchema.index({ createdAt: 1 }, { expireAfterSeconds: 60 * 60 * 24 * 7 });
-passwordResetSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model("PasswordReset", passwordResetSchema);
