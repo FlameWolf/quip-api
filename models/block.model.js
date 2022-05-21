8da6ddd3c27a3d1f63f7328cb190c4d869a7effa
FlameWolf <agnijith@gmail.com>
2022-05-21 11:35:03 +0530

    Add `reason` field

--- models/block.model.js
+++ models/block.model.js
@@ -2,12 +2,21 @@
 
 const { ObjectId } = require("bson");
 const mongoose = require("mongoose");
+const { contentLengthRegExp, maxContentLength } = require("../library");
 const uniqueValidator = require("mongoose-unique-validator");
 
 const blockSchema = new mongoose.Schema(
 	{
 		user: { type: ObjectId, ref: "User", required: true, index: true },
-		blockedBy: { type: ObjectId, ref: "User", required: true }
+		blockedBy: { type: ObjectId, ref: "User", required: true },
+		reason: {
+			type: String,
+			trim: true,
+			validate: {
+				validator: value => value.match(contentLengthRegExp).length <= maxContentLength,
+				message: "Reason length exceeds the maximum allowed limit"
+			}
+		}
 	},
 	{
 		timestamps: {
