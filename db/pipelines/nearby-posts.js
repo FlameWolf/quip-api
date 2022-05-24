42804a931ff94b117da16cbfc6e40266d364d994
FlameWolf <agnijith@gmail.com>
2022-05-24 20:04:11 +0530

    Create nearby posts aggregation pipeline

---
+++ db/pipelines/nearby-posts.js
@@ -0,0 +1,56 @@
+"use strict";
+
+const postAggregationPipeline = require("./post");
+
+const nearbyPostsAggregationPipeline = (coordinates, maxDistance = 5000, userId = undefined, lastDistance = undefined, lastPostId = undefined) => {
+	const pageConditions = {};
+	if (lastDistance && lastPostId) {
+		const parsedLastDistance = parseFloat(lastDistance);
+		Object.assign(pageConditions, {
+			$expr: {
+				$or: [
+					{
+						$and: [
+							{
+								$eq: ["$distance", parsedLastDistance]
+							},
+							{
+								$lt: ["$_id", ObjectId(parsedLastDistance)]
+							}
+						]
+					},
+					{
+						$lt: ["$distance", parsedLastDistance]
+					}
+				]
+			}
+		});
+	}
+	return [
+		{
+			$geoNear: {
+				near: {
+					type: "Point",
+					coordinates
+				},
+				maxDistance,
+				distanceField: "distance"
+			}
+		},
+		{
+			$sort: {
+				distance: 1,
+				createdAt: -1
+			}
+		},
+		{
+			$match: pageConditions
+		},
+		{
+			$limit: 20
+		},
+		...postAggregationPipeline(userId)
+	];
+};
+
+module.exports = nearbyPostsAggregationPipeline;
