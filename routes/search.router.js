9ac6abce35d0557e1ab0cf86e06d05d1d8c7036b
FlameWolf <agnijith@gmail.com>
2022-05-25 10:43:09 +0530

    Add `/nearby` route

--- routes/search.router.js
+++ routes/search.router.js
@@ -5,5 +5,6 @@ const router = express.Router();
 const searchController = require("../controllers/search.controller");
 
 router.get("/", searchController.searchPosts);
+router.get("/nearby", searchController.nearbyPosts);
 
 module.exports = router;
