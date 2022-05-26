74543b60dbca647ee8a5fa187bc3468d47de8b79
FlameWolf <agnijith@gmail.com>
2022-05-26 10:50:38 +0530

    If a post is a repeat then bookmark the repeated post

--- controllers/bookmarks.controller.js
+++ controllers/bookmarks.controller.js
@@ -1,18 +1,19 @@
 "use strict";
 
-const Post = require("../models/post.model");
+const postsController = require("./posts.controller");
 const Bookmark = require("../models/bookmark.model");
 
 const addBookmark = async (req, res, next) => {
 	const postId = req.params.postId;
 	const userId = req.userInfo.userId;
 	try {
-		if (!(await Post.countDocuments({ _id: postId }))) {
+		const post = await postsController.findPostById(postId);
+		if (!post) {
 			res.status(404).send("Post not found");
 			return;
 		}
 		const bookmarked = await new Bookmark({
-			post: postId,
+			post: post._id,
 			bookmarkedBy: userId
 		}).save();
 		res.status(200).json({ bookmarked });
