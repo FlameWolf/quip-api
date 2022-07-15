c2658ec11de425d629150e3ac050361343137b6e
FlameWolf <agnijith@gmail.com>
2022-07-15 14:37:13 +0530

    Remove `/sign-out` route

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -8,6 +8,5 @@ router.post("/sign-up", authController.signUp);
 router.post("/sign-in", authController.signIn);
 router.post("/refresh-token", authController.refreshAuthToken);
 router.get("/revoke-token/:token", authController.revokeRefreshToken);
-router.get("/sign-out", authController.signOut);
 
 module.exports = router;
