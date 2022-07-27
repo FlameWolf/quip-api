b9a76c5c767a0efdb5481d5705dd793c717cb8ff
FlameWolf <agnijith@gmail.com>
2022-07-27 16:28:35 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,80 @@
+"use strict";
+
+const express = require("express");
+const isNotProdEnv = process.env.NODE_ENV !== "production";
+if (isNotProdEnv) {
+	require("dotenv").config();
+}
+const http = require("http");
+const jwt = require("jsonwebtoken");
+const expressOasGenerator = (() => {
+	if (isNotProdEnv) {
+		return require("express-oas-generator");
+	}
+})();
+require("./patch-router-param");
+require("./schemaTypes/url");
+require("./schemaTypes/point");
+
+require("mongoose")
+	.connect(process.env.DB_CONNECTION)
+	.then(() => {
+		console.log("Connected to the database");
+	})
+	.catch(() => {
+		console.log("Unable to connect to the database");
+	});
+require("cloudinary").v2.config({
+	cloud_name: process.env.CLOUD_BUCKET,
+	api_key: process.env.CLOUD_API_KEY,
+	api_secret: process.env.CLOUD_API_SECRET
+});
+
+const app = express();
+app.use(require("helmet")());
+app.use(async (req, res, next) => {
+	res.setHeader("Access-Control-Allow-Origin", process.env.ALLOW_ORIGIN);
+	res.setHeader("Access-Control-Allow-Credentials", true);
+	res.setHeader("Access-Control-Allow-Headers", "Authorization, Origin, X-Requested-With, Content-Type, Accept, X-Slug, X-UID");
+	res.setHeader("Access-Control-Allow-Methods", "OPTIONS, POST, PUT, PATCH, GET, DELETE");
+	if (req.method === "OPTIONS") {
+		res.status(200).send();
+		return;
+	}
+	next();
+});
+app.use(express.json());
+if (isNotProdEnv) {
+	expressOasGenerator.handleResponses(app, {
+		predefinedSpec: require("./swagger.json"),
+		specOutputFileBehavior: "RECREATE",
+		swaggerDocumentOptions: {
+			customCss: ".wrapper > .block > div > span:first-child, section.models.is-open { display: none; }"
+		}
+	});
+}
+app.use(async (req, res, next) => {
+	try {
+		const authToken = req.headers.authorization?.replace(/^bearer\s+/i, "");
+		req.userInfo = authToken && jwt.verify(authToken, process.env.JWT_AUTH_SECRET);
+	} catch (err) {}
+	next();
+});
+app.use("/", require("./routes/index.router"));
+app.use("/auth", require("./routes/auth.router"));
+app.use("/users", require("./routes/users.router"));
+app.use("/lists", require("./routes/lists.router"));
+app.use("/posts", require("./routes/posts.router"));
+app.use("/search", require("./routes/search.router"));
+app.use("/settings", require("./routes/settings.router"));
+app.use(async (err, req, res, next) => {
+	res.status(500).send(err);
+});
+if (isNotProdEnv) {
+	expressOasGenerator.handleRequests();
+}
+
+const server = http.createServer(app);
+server.listen(+process.env.PORT || 4096, () => {
+	console.log(`Listening on ${server.address().port}`);
+});
